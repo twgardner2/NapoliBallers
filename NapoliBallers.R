6d870fc0b8035e7bb81e1111d69afcb758bf912e
Tom Gardner <twgardner2@gmail.com>
2017-08-26 12:55:05 +0200

    Added jQuery DataTables

--- NapoliBallers.R
+++ NapoliBallers.R
@@ -1,3 +1,4 @@
+library(kableExtra)
 library(readxl)
 library(stringr)
 library(ggplot2)
@@ -6,5 +7,27 @@ library(tidyverse)
 # Read in raw data
 data <- readxl::read_xlsx("NapoliBallersHistory.xlsx")
 
+# Clean column names
+names(data) <- str_to_lower(names(data))
+names(data)[names(data) == 'pf/g'] <- "pfPerGame"
+names(data)[names(data) == 'pa/g'] <- "paPerGame"
+names(data)[names(data) == 'diff'] <- "pointDiff"
+
 # Check owner names are unique
-unique(data$Owner)
+unique(data$owner)
+table(data$owner, data$year)
+
+
+
+
+
+
+
+
+
+paPlot <- ggplot(data, aes(x=pa))
+paPlot + geom_density()
+
+
+pfPlot <- ggplot(data, aes(x=pf))
+pfPlot + geom_density() 

fdfc4fe0bcf0d0963dc5b26a6af98bb28a1b4704
Tom Gardner <twgardner2@gmail.com>
2017-08-26 09:53:59 +0200

    Removed absolute path reference

--- NapoliBallers.R
+++ NapoliBallers.R
@@ -4,8 +4,7 @@ library(ggplot2)
 library(tidyverse)
 
 # Read in raw data
-path <- "C://Users//Tom//Documents//R//NapoliBallers//"
-data <- readxl::read_xlsx(str_c(path, "NapoliBallersHistory.xlsx"))
+data <- readxl::read_xlsx("NapoliBallersHistory.xlsx")
 
 # Check owner names are unique
 unique(data$Owner)

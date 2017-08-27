5c191e20793d5bfdb0ef4100f441c77d1866c44d
Tom Gardner <twgardner2@gmail.com>
2017-08-27 22:49:35 +0200

    Started 'points for' plot, some HTML, and starting to add d3js

--- NapoliBallers.R
+++ NapoliBallers.R
@@ -1,11 +1,13 @@
+library(RColorBrewer)
 library(kableExtra)
 library(readxl)
 library(stringr)
 library(ggplot2)
 library(tidyverse)
+library(knitr)
 
 # Read in raw data
-data <- readxl::read_xlsx("NapoliBallersHistory.xlsx", col_types = )
+data <- readxl::read_xlsx("NapoliBallersHistory.xlsx")
 
 # Clean columns
 names(data) <- str_to_lower(names(data))
@@ -14,8 +16,14 @@ names(data)[names(data) == 'pa/g'] <- "paPerGame"
 names(data)[names(data) == 'diff'] <- "pointDiff"
 data$year <- as.character(data$year)
 
+
+
 # Check owner names are unique
 unique(data$owner)
+
+
+
+
 
 # Make owner participation table
 ownerTable <- data %>% 
@@ -40,21 +48,36 @@ for (col in 3:ncol(ownerTable)) {
   }
 }
 
+kable(ownerTable, table.attr = "id=\"ownertable\"")
+
 
 
 # Summarize results
 rankTable <- data %>% count(owner, rank)  %>% filter(rank<=3)
 rankTable <- rankTable %>% spread(key = rank, value = n, fill = 0)
-newNames <- c("Owner", "1st Place", "2nd Place", "3rd Place")
+newNames <- c("owner", "1st Place", "2nd Place", "3rd Place")
 colnames(rankTable) <- newNames
-colnames(rankTable)
 
-# Wins/Losses/Ties table
+winsTable <- data %>% group_by(owner) %>% summarize(Wins = sum(w), Losses = sum(l), Ties = sum(tie))  
+
+performanceTable <- full_join(rankTable, winsTable, by="owner") %>% mutate_all(funs(replace(., which(is.na(.)), 0)))
+
+kable(performanceTable, table.attr = "id=\"performancetable\"")
 
 
-# paPlot <- ggplot(data, aes(x=pa))
-# paPlot + geom_density()
-# 
-# 
-# pfPlot <- ggplot(data, aes(x=pf))
-# pfPlot + geom_density() 
+
+pfData <- data %>% filter(!is.na(pfPerGame))
+pfPlot <- ggplot(pfData, aes(x=pfPerGame, fill=owner))
+pfPlot + geom_histogram(aes(color=factor(ifelse(rank==1,1,0)))) + scale_color_manual(values = c("black", "red"))
+
+#+ scale_fill_brewer(length(unique(data$owner)), palette="Spectral")
+
+
+
+paPlot + geom_density()
+paPlot + geom_bar()
+
+
+
+pfPlot <- ggplot(data, aes(x=pf))
+pfPlot + geom_density() 
